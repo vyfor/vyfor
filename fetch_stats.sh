2f800e16e2295ee456296ed2e7df95b61de4fd4e
vyfor <92883017+vyfor@users.noreply.github.com>
2025-01-19 16:22:43 +0500

    Update fetch_stats.sh

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,4 +1,4 @@
-wget "https://readme-stats-nu.vercel.app/api?username=vyfor&show_icons=true&theme=github_dark&hide_border=true&line_height=27&include_all_commits=false" -O assets/stats_dark.svg
-wget "https://readme-stats-nu.vercel.app/api?username=vyfor&show_icons=true&theme=default&hide_border=true&line_height=27&include_all_commits=false" -O assets/stats_light.svg
+wget "https://readme-stats-nu.vercel.app/api?username=vyfor&show_icons=true&theme=github_dark&hide_border=true&line_height=27&hide=commits,contribs&show=reviews,prs_merged" -O assets/stats_dark.svg
+wget "https://readme-stats-nu.vercel.app/api?username=vyfor&show_icons=true&theme=default&hide_border=true&line_height=27&hide=commits,contribs&show=reviews,prs_merged" -O assets/stats_light.svg
 wget "https://readme-stats-nu.vercel.app/api/top-langs/?username=vyfor&hide=javascript,java&exclude_repo=dive-bot,axeon-bot,yu-bot,cordex-kord&langs_count=3&theme=github_dark&hide_border=true" -O assets/languages_dark.svg
 wget "https://readme-stats-nu.vercel.app/api/top-langs/?username=vyfor&hide=javascript,java&exclude_repo=dive-bot,axeon-bot,yu-bot,cordex-kord&langs_count=3&theme=default&hide_border=true" -O assets/languages_light.svg
